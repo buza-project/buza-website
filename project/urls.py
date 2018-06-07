7d33a2c22fdf2eae2d4644dd8674a6521f718e74
sewagodimo <edneciamat@gmail.com>
2018-06-07 21:02:28 +0200

    .pyc files being rude

--- project/urls.py
+++ project/urls.py
@@ -39,7 +39,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)

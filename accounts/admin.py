ed1626f85ae8623e64fc99a4a3999617a3497560
sewagodimo <edneciamat@gmail.com>
2018-06-18 14:19:11 +0200

    restructured the code structure

--- accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-
-
-class ProfileAdmin(admin.ModelAdmin):
-	# Register your models here.
-	list_display = ['user', 'school', 'reputation']
-admin.site.register(Profile, ProfileAdmin)
---
+++ project/boards/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Question
+
+
+class QuestionSerializer(serializers.Serializer):
+	class Meta:
+		model = Question
+		fields = "__all__"

a715930aa607aecd7c2838e5b9505bbb7f949b02
Pi Delport <pjdelport@gmail.com>
2018-07-08 22:05:15 +0200

    Add django-environ settings module: buza.settings_env

---
+++ src/buza/settings_env.py
@@ -0,0 +1,23 @@
+"""
+Django settings module for buza-website using django-environ.
+"""
+import environ
+
+from buza.settings_base import *  # noqa: F401
+
+
+env = environ.Env()
+
+
+DEBUG = env('DJANGO_DEBUG')
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+
+DATABASES = {
+    'default': env.db('DJANGO_DATABASE_URL'),
+}
+
+STATIC_ROOT = env.path('DJANGO_STATIC_ROOT')
+STATIC_URL = env('DJANGO_STATIC_URL', default='/static/')
+
+MEDIA_ROOT = env.path('DJANGO_MEDIA_ROOT')
+MEDIA_URL = env('DJANGO_MEDIA_URL', default='/media/')

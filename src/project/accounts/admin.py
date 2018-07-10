fcba5c21c501807378794b03de0311faadd61255
sewagodimo <edneciamat@gmail.com>
2018-07-10 23:16:23 +0200

    django admin: added django admin for the buza app

--- src/project/accounts/admin.py
+++ src/project/accounts/admin.py
@@ -5,7 +5,7 @@ from project.accounts.models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
     # Register your models here.
-    list_display = ['user', 'school', 'reputation']
+    list_display = ['user', 'school', 'grade','is_active']
 
 
 admin.site.register(Profile, ProfileAdmin)

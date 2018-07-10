3200578b8cbb12422a15b3f011d85fc81289e143
sewagodimo <edneciamat@gmail.com>
2018-07-10 23:01:53 +0200

    managers: removed unncesarry import

--- src/buza/managers.py
+++ src/buza/managers.py
@@ -1,7 +1,8 @@
+import re
+
 from django.contrib.auth.base_user import BaseUserManager
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
-import re
 
 
 def validate_phone(phone):

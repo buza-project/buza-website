94c5e8b94f8b1022278171e3831b1916f2d4102f
Pi Delport <pjdelport@gmail.com>
2018-07-12 19:50:40 +0200

    Add missing _

--- src/buza/models.py
+++ src/buza/models.py
@@ -59,7 +59,7 @@ class BuzaUser(AbstractBaseUser, PermissionsMixin):
 
     class Meta:
         verbose_name = _("user")
-        verbose_name_plural = ("users")
+        verbose_name_plural = _("users")
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user)

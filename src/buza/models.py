9a651356d0727c7747e63c4e01949ec89e08ad9a
Pi Delport <pjdelport@gmail.com>
2018-07-12 22:14:36 +0200

    (Add type)

--- src/buza/models.py
+++ src/buza/models.py
@@ -60,5 +60,5 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name = _("user")
         verbose_name_plural = _("users")
 
-    def __str__(self):
-        return self.username
+    def __str__(self) -> str:
+        return str(self.username)

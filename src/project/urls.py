01433dbf4b65cadea480efe1196a4896edc921ec
Pi Delport <pjdelport@gmail.com>
2018-07-13 05:19:32 +0200

    Development settings: Seve media from MEDIA_URL, not STATIC_URL
    This is needed for the URLs generated by default to work.

--- src/project/urls.py
+++ src/project/urls.py
@@ -39,4 +39,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

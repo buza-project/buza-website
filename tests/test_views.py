e0f9a860ae7c953cf4dbde61c6bb499cf95cd078
Pi Delport <pjdelport@gmail.com>
2018-07-13 05:06:28 +0200

    Minimally user detail template

--- tests/test_views.py
+++ tests/test_views.py
@@ -13,7 +13,16 @@ class TestUserDetail(TestCase):
         assert HTTPStatus.NOT_FOUND == response.status_code
 
     def test_get(self) -> None:
-        user = models.User.objects.create()
+        user = models.User.objects.create(
+            first_name='Test',
+            last_name='User',
+            email='tester@example.com',
+            bio='Example bio.',
+        )
         response = self.client.get(reverse('user-detail', kwargs=dict(pk=user.pk)))
         assert HTTPStatus.OK == response.status_code
         self.assertTemplateUsed(response, 'buza/user_detail.html')
+
+        self.assertContains(response, 'Test User', count=2)
+        self.assertContains(response, 'Email: tester@example.com', count=1)
+        self.assertContains(response, 'Bio: Example bio.', count=1)

2c5f5fcfe39a7e6975a7160f07b4fb1903fa6077
Pi Delport <pjdelport@gmail.com>
2018-07-13 05:14:47 +0200

    (Prefer absolute import)

--- src/buza/urls.py
+++ src/buza/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from . import views
+from buza import views
 
 
 urlpatterns = [

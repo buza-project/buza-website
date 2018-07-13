eb7b656659ab20620baebcc4ab94455cc8c071a6
Pi Delport <pjdelport@gmail.com>
2018-07-13 04:43:27 +0200

    User model: Test default values
    (This is not critical, but useful to track changes.)

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,5 +5,27 @@ from buza import models
 
 class TestUser(TestCase):
 
+    def test_defaults(self):
+        user = models.User.objects.create()
+        assert {
+            'bio': None,
+            'date_joined': user.date_joined,
+            'email': '',
+            'first_name': '',
+            'grade': 7,
+            'id': user.pk,
+            'is_active': True,
+            'is_staff': False,
+            'is_superuser': False,
+            'last_login': None,
+            'last_name': '',
+            'password': '',
+            'phone': '',
+            'photo': '',
+            'school': None,
+            'school_address': None,
+            'username': '',
+        } == models.User.objects.filter(pk=user.pk).values().get()
+
     def test_repr(self):
         assert '<User: test>' == repr(models.User(username='test'))

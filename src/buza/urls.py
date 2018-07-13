84a5a746d7f319f6b6d09463e91bedb52fbd3eeb
Pi Delport <pjdelport@gmail.com>
2018-07-13 06:12:17 +0200

    URLs: Include django.contrib.auth.urls, and use default template names

--- src/buza/urls.py
+++ src/buza/urls.py
@@ -1,6 +1,7 @@
+import django.contrib.auth.urls
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from django.views import generic
 
 import project.urls
 from buza import views
@@ -11,39 +12,7 @@ urlpatterns = [
     url(r'^edit/$', views.edit, name='edit'),
     path(r'users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
 
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', views.logged_out, name='logout'),
-    url(r'^password_change/$', views.password_change, name='password_change'),
-    url(
-        r'^password_change/done/$',
-        views.password_change_done,
-        name='password_change_done',
-    ),
-
-    url(
-        r'^password_reset/$',
-        auth_views.password_reset,
-        {'template_name': 'registration/password_reset_form_user.html'},
-        name='password_reset',
-    ),
-    url(
-        r'^password_reset/done/$',
-        auth_views.password_reset_done,
-        {'template_name': 'registration/password_reset_done_index.html'},
-        name='password_reset_done',
-    ),
-    url(
-        r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
-        auth_views.password_reset_confirm,
-        {'template_name': 'registration/password_reset_confirm_user.html'},
-        name='password_reset_confirm',
-    ),
-    url(
-        r'^password-reset/complete/$',
-        auth_views.password_reset_complete,
-        {'template_name': 'registration/password_reset_complete_user.html'},
-        name='password_reset_complete',
-    ),
+    path('auth/', include(django.contrib.auth.urls)),
 
     # Fall back to older project urls.
     path('', include(project.urls)),  # TODO: Migrate

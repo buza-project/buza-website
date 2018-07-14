a51112a8d13de521759846f93861743b3c813505
Pi Delport <pjdelport@gmail.com>
2018-07-14 14:51:18 +0200

    Remove some unused code

--- src/project/boards/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Question
-
-
-class QuestionSerializer(serializers.Serializer):
-    class Meta:
-        model = Question
-        fields = "__all__"

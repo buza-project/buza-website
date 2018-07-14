9d29d703222a6b05d47d25bf556955a1d625afc3
Pi Delport <pjdelport@gmail.com>
2018-07-14 18:33:52 +0200

    Add admin for answers

--- src/buza/admin.py
+++ src/buza/admin.py
@@ -8,6 +8,14 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'school', 'grade']
 
 
+class AnswerInline(admin.TabularInline):
+    extra = 0
+    model = models.Answer
+
+    fields = ['author', 'body']
+    raw_id_fields = ['author']
+
+
 @admin.register(models.Question)
 class QuestionAdmin(admin.ModelAdmin):
     date_hierarchy = 'created'
@@ -16,4 +24,22 @@ class QuestionAdmin(admin.ModelAdmin):
     search_fields = ['title', 'author__username']
 
     raw_id_fields = ['author']
+    readonly_fields = ['created', 'modified']
+
+    inlines = [AnswerInline]
+
+
+@admin.register(models.Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    ordering = ['-created']
+    list_display = ['body', 'author', 'question', 'created']
+    search_fields = [
+        'body',
+        'author__username',
+        'question__author__username',
+        'question__title',
+    ]
+
+    raw_id_fields = ['author', 'question']
     readonly_fields = ['created', 'modified']

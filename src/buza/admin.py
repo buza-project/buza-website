40ee987522c714cf6e1ecf7b5158abddae78b35a
Pi Delport <pjdelport@gmail.com>
2018-07-14 15:56:52 +0200

    Add Question admin

--- src/buza/admin.py
+++ src/buza/admin.py
@@ -6,3 +6,14 @@ from buza import models
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'school', 'grade']
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    ordering = ['-created']
+    list_display = ['title', 'author', 'created']
+    search_fields = ['title', 'author__username']
+
+    raw_id_fields = ['author']
+    readonly_fields = ['created', 'modified']

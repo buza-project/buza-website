4424b842c629c64454705d64d2305f20cd4e81a8
Pi Delport <pjdelport@gmail.com>
2018-07-15 00:05:49 +0200

    Update User admin to extend Django's base UserAdmin

--- src/buza/admin.py
+++ src/buza/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from buza import models
 
 
 @admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'school', 'grade']
+class UserAdmin(DjangoUserAdmin):
+    """
+    Extend the base Django UserAdmin with support for some Buza fields.
+    """
+
+    list_display = list(DjangoUserAdmin.list_display) + ['grade']
+    list_filter = list(DjangoUserAdmin.list_filter) + ['grade']
+
+    fieldsets = list(DjangoUserAdmin.fieldsets[:1]) + [
+        (_('Buza fields'), {'fields': [
+            'phone',
+            'grade',
+            'photo',
+            'bio',
+        ]}),
+    ] + list(DjangoUserAdmin.fieldsets[1:])

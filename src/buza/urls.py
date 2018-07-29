f8de9607c8a6d5992df8b50e9631c7cb2da5a89a
sewagodimo <edneciamat@gmail.com>
2018-07-29 13:11:58 +0200

    re arranged urls

--- src/buza/urls.py
+++ src/buza/urls.py
@@ -10,6 +10,24 @@ from buza import views
 urlpatterns = [
     path('', generic.RedirectView.as_view(pattern_name='question-list'), name='home'),
 
+    # Admin paths
+    url(r'^admin/', admin.site.urls),
+
+    # user related paths
+    url(r'^register/$', views.UserCreate.as_view(), name='register'),
+    url(r'^edit/$', views.edit, name='edit'),
+    path(r'users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+
+    # subject related paths
+    path('subjects/<int:pk>/', views.SubjectDetail.as_view(), name='subject-detail'),
+    path(
+        'subjects/my-subjects/<int:pk>/',
+        views.UserSubjectList.as_view(),
+        name='my-subject-list',
+    ),
+    path('subjects/', views.SubjectList.as_view(), name='subject-list'),
+
+    # question related paths
     path('questions/', views.QuestionList.as_view(), name='question-list'),
     path('questions/<int:pk>/', views.QuestionDetail.as_view(), name='question-detail'),
     path('questions/ask/', views.QuestionCreate.as_view(), name='question-create'),
@@ -19,6 +37,7 @@ urlpatterns = [
         name='question-edit',
     ),
 
+    # answer related paths
     path(
         'questions/<int:question_pk>/answer/<int:pk>/edit',
         views.AnswerUpdate.as_view(),
@@ -30,22 +49,8 @@ urlpatterns = [
         views.AnswerCreate.as_view(),
         name='answer-create',
     ),
-    path('subjects/<int:pk>/', views.SubjectDetail.as_view(), name='subject-detail'),
-    path(
-        'subjects/my-subjects/<int:pk>/',
-        views.UserSubjectList.as_view(),
-        name='my-subject-list',
-    ),
-    path('subjects/', views.SubjectList.as_view(), name='subject-list'),
-
-    url(r'^register/$', views.UserCreate.as_view(), name='register'),
-    url(r'^edit/$', views.edit, name='edit'),
-    path(r'users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
 
     path('auth/', include(django.contrib.auth.urls)),
-
-    # Use admin urls
-    url(r'^admin/', admin.site.urls),
 
     # social log
     url(r'^oauth/', include('social_django.urls', namespace='social')),

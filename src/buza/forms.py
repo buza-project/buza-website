61120650e288341ebb4f18db65b8b9ff9960659d
Pi Delport <pjdelport@gmail.com>
2018-10-14 20:12:15 +0200

    (isort)

--- src/buza/forms.py
+++ src/buza/forms.py
@@ -1,8 +1,8 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
 
 from buza.models import User
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 
 
 class UserEditForm(forms.ModelForm):
